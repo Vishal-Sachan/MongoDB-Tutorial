e0b654a906cc75db008c5f52e88cacbba2ded979
Vishal-Sachan <vishal.sachan@credextechnology.com>
2022-02-02 18:04:13 +0530

    POST methoda applied

--- src/app.js
+++ src/app.js
@@ -1,54 +1,105 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const port=process.env.PORT || 3000;
-const app=express();
-const cars=require('./cars')
+const bodyParser= require('body-parser');
+const port = process.env.PORT || 3000;
+const app = express();
+const cars = require("../models/cars");
 
-mongoose.connect("mongodb://localhost:27017/Automobiles",{})
-.then(()=>{
+app.use(bodyParser.json());
+
+mongoose.connect("mongodb://localhost:27017/Automobiles", {})
+  .then(() => {
     console.log("Database Connected");
-})
-.catch(()=>{
+  })
+  .catch(() => {
     console.log("Database Failed");
-})
+  });
 
-app.get('/',(req,res)=>{
-    res.send("<h1>Welcome to MongoDB Tutorial</h1>")
-})
+app.get("/", (_,res) => {
+  res.send("<h1>Welcome to MongoDB Tutorial</h1>");
+});
 
-app.get('/cars',(req,res)=>{
-    cars.find((err,data)=>{
-        if(err){
-            console.log("can't fetch");
-        }
-        else{
-            res.send(data);
-        }
-    }) 
-})
-
-app.get('/cars/active=:id',(req,res)=>{
-    cars.find({active:req.params.id},(err,data)=>{
-        if(err){
-            console.log("can't fetch");
-        }
-        else{
-            res.send(data);
-        }
+app.post("/cars/create",
+async(req,res)=>{
+  try{
+    const car= req.body;
+    const response= await cars.create(car);
+    console.log(response);
+    res.json({
+      status:"ok"
+      
     })
+  }
+  catch(err){
+    console.log(err);
+  }
 })
 
-app.get('/cars/name=:id',(req,res)=>{
-    cars.find({name:req.params.id},(err,data)=>{
-        if(err){
-            console.log("can't fetch");
-        }
-        else{
-            res.send(data);
-        }
+app.post("/cars/update", 
+async(req,res)=>{
+  try{
+    const updateCar=req.body;
+    const response= await cars.updateOne({name:updateCar.name},{$set:{type:updateCar.type}});
+    console.log(response);
+    res.json({
+      status:"ok"
+      
     })
+  }
+  catch(err){
+    console.log(err);
+  }
 })
 
-app.listen(port,()=>{
-    console.log(`Listening at ${port}...`);
+app.post("/cars/delete",
+async(req,res)=>{
+  try{
+    const deleteCar=req.body;
+    const response= await cars.deleteOne({model:deleteCar.model});
+    console.log(response);
+    res.json({
+      status:"ok",
+      deleted:`${deleteCar.name}`
+      
+    })
+  }
+  catch(err){
+    console.log(err);
+    
+  }
 })
+
+app.get("/cars",
+  async(_, res) =>{ 
+    try{
+       const data = await cars.find();
+    res.send(data);
+    }
+    catch(err){
+      console.log(err);
+  }
+});
+
+app.get("/cars/active=:id", (req, res) => {
+  cars.find({ active: req.params.id }, (err, data) => {
+    if (err) {
+      console.log("can't fetch");
+    } else {
+      res.send(data);
+    }
+  });
+});
+
+app.get("/cars/name=:id", (req, res) => {
+  cars.find({ name: req.params.id }, (err, data) => {
+    if (err) {
+      console.log("can't fetch");
+    } else {
+      res.send(data);
+    }
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Listening at ${port}...`);
+});
