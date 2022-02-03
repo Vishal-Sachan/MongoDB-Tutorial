3bac4f82b64a280984864c2b1bdf76028278cc88
Vishal-Sachan <vishal.sachan@credextechnology.com>
2022-02-03 12:41:12 +0530

    schema updated

--- models/cars.js
+++ models/cars.js
@@ -1,24 +1,45 @@
 const mongoose = require("mongoose");
 
 const carSchema = new mongoose.Schema({
-    name: String,
-    model: String,
+  name: {
     type: String,
-    color: String,
-    number: String,
-    active: Boolean,
-    price: Number,
-    createdAt: {
-        type: Date,
-        default: function () {
-          return Date.now();
-        }
-      },
-    updatedAt:{
-        type: Date,
-        default: function (){
-            return Date.now();
-        }
-        }
+    required: true,
+  },
+  model: {
+    type: String,
+  },
+  type: {
+    type: String,
+  },
+  color: {
+    type: String,
+  },
+  number: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  active: {
+    type: Boolean,
+    required: true,
+  },
+  price: {
+    type: Number,
+  },
+  //created_at : { type: Date, required: true, default: Date.now },
+  createdAt: {
+    type: Date,
+    default: function () {
+      return Date.now();
+    },
+  },
+  updatedAt: {
+    type: Date,
+    default: function () {
+      return Date.now();
+    },
+  },
+  //timestamps:{createdAt:'created_at', updatedAt:'updated_at'}
+  //timestamps: true
 });
-module.exports=mongoose.model("cars", carSchema);
+module.exports = mongoose.model("cars", carSchema);
